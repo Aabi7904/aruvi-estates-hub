f20e6e2c885e71d1c0bbb173e704c033401b7a43
Harish Aabitheen J <harishaabitheen7904@gmail.com>
2026-02-17 10:40:31 +0530

    added half plots for sivan 1

--- src/data/maps/sivanmalai2plot.ts
+++ src/data/maps/sivanmalai2plot.ts
@@ -5,142 +5,159 @@ import { PlotPosition } from "./mapRegistry";
 export const sivanmalai2plotPositions: PlotPosition[] = [
   {
     id: "1",
-    path: "M55.0106 0.530755L3.01062 3.53076L0.51062 111.031L51.0106 116.031L55.0106 0.530755Z", // Paste 'd' here
+    path: "M54.5128 0.5H3.51282L0.512817 110L51.5128 114L54.5128 0.5Z", // Paste 'd' here
     x: 1021, // Paste Positive X here
     y: 1049.5  // Paste Positive Y here
   },
    {
     id: "12",
-    path: "M58.0132 0.525772L7.01324 3.02577L4.51324 98.5258H55.5132L58.0132 0.525772Z", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    path: "M52.0162 96.5L0.516235 99L3.51624 0.5H56.0162L52.0162 96.5Z", // Paste 'd' here
+    x: 1024, // Paste Positive X here
+    y: 952.5  // Paste Positive Y here
   },
   {
     id: "2",
-    path: "M50.5112 0.5H3.01123L0.51123 105L46.5112 108.5L50.5112 0.5Z", // Paste 'd' here
-    x: 974, // Paste Positive X here
-    y: 1053  // Paste Positive Y here
-  },
-  {
-    id: "3",
-    path: "M49.0162 0.535457L4.01617 3.53546L0.516174 103.035L46.0162 107.535L49.0162 0.535457Z", // Paste 'd' here
-    x: 927.5, // Paste Positive X here
+    path: "M50.5199 0.5H5.0199L0.519897 106.5L47.5199 110.5L50.5199 0.5Z", // Paste 'd' here
+    x: 973.5, // Paste Positive X here
     y: 1051.5  // Paste Positive Y here
   },
   {
-    id: "4",
-    path: "M49.5152 0.534557L3.5152 3.53456L0.515198 96.5346L47.0152 100.035L49.5152 0.534557Z", // Paste 'd' here
-    x: 880.5, // Paste Positive X here
+    id: "3",
+    path: "M48.5117 0.5H3.01166L0.511658 101L45.5117 104.5L48.5117 0.5Z", // Paste 'd' here
+    x: 928.5, // Paste Positive X here
     y: 1053  // Paste Positive Y here
   },
   {
+    id: "4",
+    path: "M49.5172 0.5H4.01715L0.517151 95.5L46.5172 99.5L49.5172 0.5Z", // Paste 'd' here
+    x: 881, // Paste Positive X here
+    y: 1054  // Paste Positive Y here
+  },
+  {
     id: "5",
-    path: "M50.0128 0.5H3.01276L0.512756 89.5L47.0128 94.5L50.0128 0.5Z", // Paste 'd' here
-    x: 834.5, // Paste Positive X here
-    y: 1056  // Paste Positive Y here
+    path: "M50.513 0.516474L3.013 2.01647L0.513 91.5165L48.013 95.5165L50.513 0.516474Z", // Paste 'd' here
+    x: 833.5, // Paste Positive X here
+    y: 1054  // Paste Positive Y here
   },
   {
     id: "6",
-    path: "M48.0197 0.5H4.01965L0.519653 84L48.0197 88V0.5Z", // Paste 'd' here
+    path: "M48.5135 0.5H3.01355L0.51355 85.5L46.5135 90L48.5135 0.5Z", // Paste 'd' here
     x: 787.5, // Paste Positive X here
-    y: 1057  // Paste Positive Y here
+    y: 1056  // Paste Positive Y here
   },
   {
     id: "7",
-    path: "M50.0153 0.5H3.51526L0.515259 100H46.0153L50.0153 0.5Z", // Paste 'd' here
-    x: 790.5,
-    y: 956  // Paste Positive Y here
+    path: "M48.5151 0.5H3.51514L0.515137 101H45.5151L48.5151 0.5Z", // Paste 'd' here
+    x: 791,
+    y: 955.5  // Paste Positive Y here
   },
   {
     id: "8",
-    path: "M50.5191 0.528625L4.0191 3.02863L0.519104 102.529L47.0191 99.5286L50.5191 0.528625Z", // Paste 'd' here
-    x: 837, // Paste Positive X here
-    y: 954  // Paste Positive Y here
-  },
-  {
-    id: "9",
-    path: "M49.0154 0.5H3.51538L0.515381 99.5H45.5154L49.0154 0.5Z", // Paste 'd' here
-    x: 885, // Paste Positive X here
-    y: 955  // Paste Positive Y here
-  },
-  {
-    id: "10",
-    path: "M48.0131 0.5H3.01312L0.513123 97H45.0131L48.0131 0.5Z", // Paste 'd' here
-    x: 931.5, // Paste Positive X here
+    path: "M51.0183 0.5H4.01831L0.518311 102L48.0183 100L51.0183 0.5Z", // Paste 'd' here
+    x: 836.5, // Paste Positive X here
     y: 954.5  // Paste Positive Y here
   },
   {
+    id: "9",
+    path: "M48.5131 0.5H3.01312L0.513123 101.5L46.5131 99.5L48.5131 0.5Z", // Paste 'd' here
+    x: 885, // Paste Positive X here
+    y: 953.5  // Paste Positive Y here
+  },
+  {
+    id: "10",
+    path: "M50.5154 0.5H3.51538L0.515381 99.5H47.0154L50.5154 0.5Z", // Paste 'd' here
+    x: 931, // Paste Positive X here
+    y: 953.5  // Paste Positive Y here
+  },
+  {
     id: "11",
-    path: "M51.0211 0.52877L4.52106 3.02877L0.521057 100.029H47.0211L51.0211 0.52877Z", // Paste 'd' here
-    x: 977.5, // Paste Positive X here
-    y: 951.5  // Paste Positive Y here
+    path: "M50.5181 0.5H4.01807L0.518066 99H48.5181L50.5181 0.5Z", // Paste 'd' here
+    x: 977, // Paste Positive X here
+    y: 952.5  // Paste Positive Y here
   },
   {
-    id: "12",
-    path: "M55.5252 0.526329L5.02521 3.02633L0.525208 100.026L52.0252 97.0263L55.5252 0.526329Z", // Paste 'd' here
-    x: 1023.5, // Paste Positive X here
-    y: 951 // Paste Positive Y here
+    id: "20",
+    path: "M97 0.507584L0.5 2.00758V55.5076L12.5 66.0076L99 64.5076L97 0.507584Z", // Paste 'd' here
+    x: 864.5, // Paste Positive X here
+    y: 813.5 // Paste Positive Y here
+  },//
+  {
+    id: "13",
+    path: "M123.008 0.506348L0.50769 2.00635L1.50769 66.5063L121.008 65.5063L123.008 0.506348Z", // Paste 'd' here
+    x: 961.5, // Paste Positive X here
+    y: 811.5 // Paste Positive Y here
   },
   {
-    id: "12",
-    path: "", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    id: "19",
+    path: "M97.0106 0.507599L0.510559 2.0076L1.51056 49.0076L98.5106 47.5076L97.0106 0.507599Z", // Paste 'd' here
+    x: 863.5, // Paste Positive X here
+    y: 766.5 // Paste Positive Y here
   },
   {
-    id: "12",
-    path: "", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    id: "14",
+    path: "M126.016 0.508301L0.516113 2.5083L2.01611 49.0083L124.516 47.0083L126.016 0.508301Z", // Paste 'd' here
+    x:960.5 , // Paste Positive X here
+    y: 765 // Paste Positive Y here
   },
   {
-    id: "12",
-    path: "", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    id: "15",
+    path: "M128.011 0.510101L0.510559 3.0101L1.51056 50.0101L127.011 48.0101L128.011 0.510101Z", // Paste 'd' here
+    x:959.5 , // Paste Positive X here
+    y: 717 // Paste Positive Y here
   },
   {
-    id: "12",
-    path: "", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    id: "16",
+    path: "M132.011 0.507904L0.510559 2.5079L1.51056 49.5079L130.511 47.0079L132.011 0.507904Z", // Paste 'd' here
+    x:957.5, // Paste Positive X here
+    y: 670.5 // Paste Positive Y here
   },
   {
-    id: "12",
-    path: "", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    id: "17",
+    path: "M95.5105 0.505127L0.510498 1.50513L1.5105 49.0051L97.0105 47.0051L95.5105 0.505127Z", // Paste 'd' here
+    x:862.5 , // Paste Positive X here
+    y: 673 // Paste Positive Y here
   },
   {
-    id: "12",
-    path: "", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    id: "18",
+    path: "M96.5107 0.50769L0.510681 2.00769L1.51068 48.5077L97.5107 47.0077L96.5107 0.50769Z", // Paste 'd' here
+    x:863 , // Paste Positive X here
+    y: 720// Paste Positive Y here
   },
   {
-    id: "12",
-    path: "", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    id: "21",
+    path: "M119.012 0.508347L0.512085 2.50835L2.01208 64.5083L120.512 77.5083L119.012 0.508347Z", // Paste 'd' here
+    x:675.5, // Paste Positive X here
+    y: 814.5// Paste Positive Y here
   },
   {
-    id: "12",
-    path: "", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    id: "22",
+    path: "M107.515 0.503418L0.514893 2.50342L2.51489 69.5034L121.515 67.5034L120.015 11.0034L107.515 0.503418Z", // Paste 'd' here
+    x:673.5, // Paste Positive X here
+    y: 747.5// Paste Positive Y here
   },
   {
-    id: "12",
-    path: "", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    id: "23",
+    path: "M68.5024 0.514587L0.502441 2.51459L1.00244 103.515L5.50244 123.015L71.5024 130.515L68.5024 0.514587Z", // Paste 'd' here
+    x:605, // Paste Positive X here
+    y: 748.5// Paste Positive Y here
   },
-  
   {
-    id: "12",
-    path: "", // Paste 'd' here
-    x: 1025, // Paste Positive X here
-    y: 950.5  // Paste Positive Y here
+    id: "24",
+    path: "M62.0074 0.512329L0.507446 2.01233L2.00745 101.512H62.0074V0.512329Z", // Paste 'd' here
+    x:535.5, // Paste Positive X here
+    y: 750// Paste Positive Y here
+  },
+  {
+    id: "25",
+    path: "M50.5 0.5H0.5V101H52L50.5 0.5Z", // Paste 'd' here
+    x:485, // Paste Positive X here
+    y: 752// Paste Positive Y here
+  },
+  {
+    id: "26",
+    path: "M66.0034 0.513885L12.0034 2.01389L0.503418 14.0139L2.00342 102.514H67.5034L66.0034 0.513885Z", // Paste 'd' here
+    x:419, // Paste Positive X here
+    y: 751.5// Paste Positive Y here
   },
 
   // Add more plots...
@@ -156,6 +173,150 @@ export const sivanmalai2plotPlots: Plot[] = [
   },
    {
     id: "12",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+   {
+    id: "11",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "2",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+   {
+    id: "10",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+   {
+    id: "3",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "9",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "4",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "8",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "5",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "7",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "6",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "20",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "13",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "19",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "14",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "15",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "16",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "17",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "18",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "21",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "22",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "23",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "24",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "25",
+    sqft: 1200,
+    status: "Available",
+    dimensions: "40' x 30'"
+  },
+  {
+    id: "26",
     sqft: 1200,
     status: "Available",
     dimensions: "40' x 30'"
